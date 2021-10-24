211ffc0dca415afbbcd5cd1bbd47a5e7ab20566a
Samuel-Appiah761 <djclassifyd123@gmail.com>
2021-10-24 15:49:18 +0000

    2 times table and age

--- src/com/company/Looping.java
+++ src/com/company/Looping.java
@@ -1,28 +1,32 @@
 package com.company;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Looping {
 
-    /* private static int TWO = 2;
-     private static String EQUAL = " = ";
-     private static String SIGN = " x ";
-
-     public static void main(String[] args) {
-         for (int i=1; i<=12; i++){
-             System.out.println(TWO +   SIGN + i + EQUAL  + i* TWO);
- //            System.out.println(TWO + " * "+ i + " = " + TWO*i);*/
-
-    private static int FIVE = 5;
+    private static int TWO = 2;
     private static String EQUAL = " = ";
-    private static String Sign = " + ";
+    private static String SIGN = " x ";
 
     public static void main(String[] args) {
-        for (int j = 1; j <= 12; j++) ;
-        System.out.println(FIVE + Sign + EQUAL + FIVE);
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(TWO + SIGN + i + EQUAL + i * TWO);
+            System.out.println(TWO + " * " + i + " = " + TWO * i);
 
-        for (int i = 0; i < 5; i++)
-            System.out.println(i);
+            if (i <= 12) {
+                System.out.println("You are kid");
+            } else if (i > 12 && i <= 19) {
+                System.out.println("You are a teenager");
+            } else {
+                System.out.println("You are an adult");
+            }
 
-
+        }
     }
+
 }
+
+
+
 
